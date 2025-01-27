d206f08fa769f7fcd925c85185de9c83d9e4ce07
Kam <karadhak@adobe.com>
2025-01-28 10:22:44 +1100

    CORS

--- blocks/products/products.js
+++ blocks/products/products.js
@@ -15,7 +15,7 @@ import {
       const data = await response.json();
 
       const config = data.data.adventureList.items.reduce((acc, item) => {
-        acc[item.title] = item.description;
+        acc[item.title] = item.description.html;
         return acc;
       }, {});
 
